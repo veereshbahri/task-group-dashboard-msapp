a0cbf6fc29fc5d50481792d0dd27aad4c7902faf
Veeresh <veereshbahri@gmail.com>
2020-06-28 19:37:56 +0530

    ms app

---
+++ zuul-proxy-server/src/test/java/com/example/zuulproxyserver/ZuulProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuulproxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
